9182d8596538ffd0ba20054120bd21e4298bfc65
Harsha Singh <harsha.singh@nathcorp.com>
2019-09-04 12:25:50 +0530

    Added my project

---
+++ Console_calculator/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            string operand;
+            ConsoleKeyInfo choice;
+            float answer;
+            
+
+            Console.WriteLine("\t\t\tCalculator Application\n");
+            while (true)
+            {
+                Console.Write("Please enter the first integer: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Please enter an operand (+, -, /, *): ");
+                operand = Console.ReadLine();
+
+                Console.Write("Please enter the second integer: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+                
+
+                switch (operand)
+                {
+                    case "-":
+                        answer = num1 - num2;
+                        break;
+                    case "+":
+                        answer = num1 + num2;
+                        break;
+                    case "/":
+                        answer = num1 / num2;
+                        break;
+                    case "*":
+                        answer = num1 * num2;
+                        break;
+                    case "%":
+                        answer = num1 % num2;
+                        break;
+                    case "^":
+                        answer = num1 ^ num2;
+                        break;
+                 default:
+                 answer = 0;
+                break;
+            }
+
+            Console.WriteLine(num1.ToString() + " " + operand + " " + num2.ToString() + " = " + answer.ToString());
+                Console.WriteLine("\n\n Do You Want To Continue? Press (Y/N)");
+                choice = Console.ReadKey();
+                
+                Console.Clear();
+            }
+
+        }
+    }
+}
